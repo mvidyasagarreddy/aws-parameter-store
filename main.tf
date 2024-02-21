c07c15b8c5a5a5acb10257a25d78f5b856cc0dcb
Mukkara Vidya Sagar Reddy <mvsreddy2018@gmail.com>
2024-02-21 07:51:15 +0530

    Roboshop Terraform

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ variable "params" {
   default = [
     {name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.roboshopai.online:8080/", type = "String"},
     {name = "roboshop.dev.frontend.user_url", value = "http://user-dev.roboshopai.online:8080/", type = "String"},
-    {name = "roboshop.dev.frontend.cart_url", value = "http:/cart-dev.roboshopai.online:8080/", type = "String"},
+    {name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.roboshopai.online:8080/", type = "String"},
     {name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.roboshopai.online:8080/", type = "String"},
     {name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.roboshopai.online:8080/", type = "String"},
 
