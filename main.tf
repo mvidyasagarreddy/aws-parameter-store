080b374577a61f70805443617cc755725ba4dace
mvidyasagarreddy <mvsreddy2018@gmail.com>
2026-07-26 01:52:46 +0000

    aws-parameter store

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ variable "params" {
         {name = "roboshop.dev.frontend.cart_url",value = "http://cart-dev.roboshop.blog:8080",type = "String"},
         {name = "roboshop.dev.frontend.shipping_url",value = "http://shipping-dev.roboshop.blog:8080",type = "String"},
         {name = "roboshop.dev.frontend.payment_url",value = "http://payment-dev.roboshop.blog:8080",type = "String"},
-        {name = "roboshop.dev.cart.redist_host",value = "http://redis-dev.roboshop.blog",type = "String"},
+        {name = "roboshop.dev.cart.redis_host",value = "http://redis-dev.roboshop.blog",type = "String"},
         {name = "roboshop.dev.cart.catalogue_host",value = "http://catalogue-dev.roboshop.blog",type = "String"},
         {name = "roboshop.dev.cart.catalogue_port",value = "8080",type = "String"},
         {name = "roboshop.dev.catalogue.mongo",value = "true",type = "String"},
@@ -27,7 +27,7 @@ variable "params" {
         {name = "roboshop.dev.shipping.db_host",value = "mysql-dev.roboshop.blog",type = "String"},
         {name = "roboshop.dev.user.mongo",value = "true",type = "String"},
         {name = "roboshop.dev.user.redis_host",value = "redis-dev.roboshop.blog",type = "String"},
-        {name = "roboshop.dev.user.Mongo_url",value = "mongodb://mongodb-dev.roboshop.blog:27017/users",type = "String"},
+        {name = "roboshop.dev.user.mongo_url",value = "mongodb://mongodb-dev.roboshop.blog:27017/users",type = "String"},
 
         ### Passwords will be never part of git repos, Usually in organizations we create them nanually. Meaning whom ever have access they will provision these secrets in parameter store.
         { name = "roboshop.dev.payment.amqp_pass", value = "roboshop123", type = "SecureString" },
